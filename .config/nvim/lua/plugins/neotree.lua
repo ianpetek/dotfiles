1c9339735702a58875e82a90f9f3fcd1a9644c32
Ian Petek <ianpetek2004@gmail.com>
2024-05-31 13:31:00 +0200

    ssh for zsh, more shortcuts

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -1,12 +1,27 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<leader>t", "<Cmd>Neotree toggle<CR>")
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>t",
+      function()
+        local path = vim.api.nvim_buf_get_name(0)
+        local is_open
+        if path:find("neo-tree", 0, true) then
+          is_open = true
+        else
+          is_open = false
+        end
+        require('neo-tree.command').execute({
+          action = "focus",
+          source = "filesystem",
+          position = "left",
+          toggle = is_open,
+        })
+      end)
+  end,
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "lua" },
+			ensure_installed = { "lua", "c", "cpp", "python", "cmake" },
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
